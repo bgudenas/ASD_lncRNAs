82de745601a55bbe12cec697825615a77a67eb17
bgudenas <bgudena@g.clemson.edu>
2016-08-03 14:15:00 -0400

    Revised all functions for clarity and tested against UCSC cytobands with success

--- cyto_converter/cyto_converter.R
+++ cyto_converter/cyto_converter.R
@@ -20,18 +20,15 @@ cytobands$Band = paste0(cytobands$Chromosome, cytobands$Band)
 cytobands = cytobands[nchar(cytobands$Chromosome)==1, ] ## remove alt haplotypes
 
 ### CNV_ter checks for "qter"s or "pter"s which indicate entire chromosomal arms affected and outputs the entire range of the affected arm
-CNV_ter = function(band){
-    chromosome = strsplit(band,"p|q")[[1]][1]
-    if (grepl("pter",band)){
-        e_pos=max(cytobands$end[cytobands$chromosome == chromosome  & grepl(paste(chromosome,"p",sep=""),cytobands$band)])
-        s_pos=0
-        return(c(s_pos,e_pos))
-    }
-    else if (grepl("qter",band)){
-        e_pos=max(cytobands$end[cytobands$chromosome == chromosome & grepl(paste(chromosome,"q",sep=""),cytobands$band)])
-        s_pos=min(cytobands$start[cytobands$chromosome == chromosome & grepl(paste(chromosome,"q",sep=""),cytobands$band)])
-        return(c(s_pos,e_pos))
+CNV_ter = function(band, chrom){
+    if (grepl("pter", band)) {
+        e_pos = max(cytobands$End[cytobands$Chromosome == chrom  & grepl(paste0(chrom,"p"),cytobands$Band)])
+        s_pos = 0
+    }else if (grepl("qter",band)) {
+        e_pos = max(cytobands$End[cytobands$Chromosome == chrom & grepl(paste0(chrom, "q"), cytobands$Band)])
+        s_pos = min(cytobands$Start[cytobands$Chromosome == chrom & grepl(paste0(chrom, "q"), cytobands$Band)])
 }
+return(c(s_pos,e_pos))
 }
 
 ## test CNV_ter over all possible combinations of chromosomes
@@ -49,103 +46,99 @@ CNV_ter = function(band){
 #### this function deals with bands that are too specific for the reference cytobands EX 1q43.11 becomes 1q43.1
 ### if a match is not found after degeneration; CNV_ter is tried; else return "NAN"
 degenerate = function(band){
-    original=band  ## make a copy of the input
-    while (!(band %in% cytobands$band) & nchar(band)>1){
-        band =substr(band,1,nchar(band)-1)  ## removes last character from string
-        if (substr(band,nchar(band),nchar(band))==".") {  ## makes sure it does not end on a period
-            band =substr(band,1,nchar(band)-1)            ## or else it removes another character
+    original = band  ## make a copy of the input
+    while (!(band %in% cytobands$Band) & nchar(band)>1) {
+        band = substr(band, 1, nchar(band)-1)  ## removes last character from string
+        if (substr(band, nchar(band), nchar(band)) == ".") {  ## makes sure it does not end on a period
+            band = substr(band,1, nchar(band)-1)            ## or else it removes another character
         }
     }
-    if (band %in% cytobands$band){
-        s_pos=cytobands[cytobands$band==band,2]
-        e_pos=cytobands[cytobands$band==band,3]
-    }else if (grepl("pter|qter",original)){ ######## try CNV_ter (check for "q|p ters" else leave NAN)
+    if (band %in% cytobands$Band){
+        s_pos = cytobands[cytobands$Band == band, 2]
+        e_pos = cytobands[cytobands$Band == band, 3]
+    }else if (grepl("pter|qter", original)){ ######## try CNV_ter (check for "q|p ters" else leave NAN)
                 ter_check= CNV_ter(original)
                 s_pos=ter_check[1]
                 e_pos=ter_check[2]
     }else {  
-        s_pos=as.numeric("NAN")
-        e_pos = as.numeric("NAN")
+        s_pos= NA
+        e_pos = NA
     }
-    return(list(s_pos,e_pos))
+    return( c(s_pos,e_pos))
 }
 
-cyto_converter = function(cyto_bands){  ##input is a list of cytobands
-    starts_vec= vector(mode = "list", length = length(cyto_bands))
-    ends_vec= vector(mode = "list", length = length(cyto_bands))
-    chrom_vec= vector(mode = "list", length = length(cyto_bands))
-    cyto_bands=as.character(cyto_bands)
-    for (i in 1:length(cyto_bands)){
+cyto_converter = function(bands, cytobands){
+#bands is a list of cytobands for conversion (including chromosome prefix)
+#cytobands is a reference dataframe from UCSC with 1st,2nd,3rd,4th columns being Chromosome, Start, End, Band
+    bands = as.character(bands)
+    starts_vec= vector(mode = "list", length = length(bands))
+    ends_vec= vector(mode = "list", length = length(bands))
+    chrom_vec= vector(mode = "list", length = length(bands))
+    
+    for (i in 1:length(bands)){
         s_pos="NaN"
         e_pos="NaN"
-        chrom=strsplit(cyto_bands[i],"p|q")[[1]][1]
+        chrom= strsplit(bands[i],"p|q")[[1]][1] #split band on p OR q and select first element
         chrom_vec[i]=chrom
-        if (grepl("-",cyto_bands[i])){  ############# this chunk deals with joined cytobands. EX. 1q43-1q42
-            start=strsplit(cyto_bands[i],"-")[[1]][1]
-            chrom=strsplit(cyto_bands[i],"p|q")[[1]][1]
-            end=paste(chrom,strsplit(cyto_bands[i],"-")[[1]][2],sep="")
-            s_pos=cytobands[cytobands$band==start,2]
-            e_pos=cytobands[cytobands$band==end,3]
+        if (grepl("-",bands[i])){  ############# this chunk deals with joined cytobands. EX. 1q43-1q42
+            band_split = strsplit(bands[i],"-")[[1]]
+            band_start = band_split[1]
+            #chrom=strsplit(bands[i],"p|q")[[1]][1]
+            band_end = paste0(chrom, band_split[2])
+            s_pos = cytobands[cytobands$Band == band_start, 2]
+            e_pos = cytobands[cytobands$Band == band_end, 3]
             
             if (!(length(s_pos))) {
-                if (sum(grepl(start,cytobands$band[cytobands$chromosome==chrom]))!=0){
-                    matches= cytobands[grepl(start,cytobands$band),]
-                    starts_vec[i] = as.numeric(as.character(min(matches[,2])))
+                if (sum(grepl(band_start, cytobands$Band[cytobands$Chromosome==chrom]))!=0) {
+                    matches = cytobands[grepl(band_start, cytobands$Band), ]
+                    starts_vec[i] = min(matches[, 2])
                 } else  {
-                    degen_pos=degenerate(start)
-                    starts_vec[i]=as.numeric(degen_pos[1])
+                    starts_vec[i]= degenerate(band_start)[1]
                         }
-                                  } else {starts_vec[i]=as.numeric(as.character(s_pos))}
+                                  } else starts_vec[i] = s_pos
                           
             if (!(length(e_pos))) {
-                if (sum(grepl(end,cytobands$band[cytobands$chromosome==chrom]))!=0){
-                    matches=cytobands[grepl(end,cytobands$band),]
-                    ends_vec[i]=as.numeric(as.character(max(matches[,3])))
-                        } else      {
-                    
-                                degen_pos=degenerate(end)
-                                ends_vec[i]=as.numeric(degen_pos[2])
+                if (sum(grepl(end,cytobands$Band[cytobands$chromosome==chrom]))!=0){
+                    matches = cytobands[grepl(band_end, cytobands$Band), ]
+                    ends_vec[i] = max(matches[, 3])
+                        } else  {
+                                ends_vec[i]= degenerate(band_end)[2]
                                     }
-                                } else {ends_vec[i] =(as.numeric(as.character(e_pos)))}                               
-        }else if (cyto_bands[i] %in% cytobands$band == FALSE & !(grepl("-",cyto_bands[i])) ){
-            matches = cytobands[grepl(cyto_bands[i],cytobands$band[cytobands$chromosome==chrom]),]
-            if (nrow(matches)== 0){
-                degen_pos = degenerate(cyto_bands[i]) 
-                starts_vec[i] = as.numeric(degen_pos[1])
-                ends_vec[i] = as.numeric(degen_pos[2])
-                            } 
-            if (nrow(matches)!=0){
-                ends_vec[i]=as.numeric(as.character(max(matches[,3])))
-                starts_vec[i] = as.numeric(as.character(min(matches[,2])))
+                                } else ends_vec[i] = e_pos
+            
+        }else if (bands[i] %in% cytobands$Band == FALSE ){
+            matches = cytobands[grepl(bands[i], cytobands$Band), ]
+            if (nrow(matches) == 0){
+                degen_pos = degenerate(bands[i]) 
+                starts_vec[i] = degen_pos[1]
+                ends_vec[i] = degen_pos[2]
+                             
+            } else if (nrow(matches)!=0){
+                starts_vec[i] = min(matches[, 2])
+                ends_vec[i] = max(matches[, 3])
                     }                                           
         } else {           
-            s_pos=cytobands[cytobands$band==cyto_bands[i],2]
-            e_pos=cytobands[cytobands$band==cyto_bands[i],3]
-            if (length(e_pos) & (length(s_pos))){
-                starts_vec[i]=as.numeric(as.character(s_pos))
-                ends_vec[i] = as.numeric(as.character(e_pos))##end
+            s_pos = cytobands[cytobands$Band == bands[i], 2]
+            e_pos = cytobands[cytobands$Band == bands[i], 3]
+            if (length(e_pos) & (length(s_pos))) {
+                starts_vec[i] = s_pos
+                ends_vec[i] = e_pos
                 }
             }
     }
-results_df <- data.frame(cyto_bands,unlist(chrom_vec),unlist(starts_vec),unlist(ends_vec))
-colnames(results_df)<-c("cytoband","chromosome","start","end")
+results_df <- data.frame(bands, unlist(chrom_vec), unlist(starts_vec), unlist(ends_vec))
+colnames(results_df)<-c("Cytoband","Chromosome","Start","End")
 
-# WHAT IS THE CODE BELOW DOING?? ------------------------------------------
-for (i in nrow(results_df)){
-    if (is.numeric(results_df$start[i])==TRUE & is.numeric(results_df$end[i]==TRUE)){
-    if (results_df$start[i] > results_df$end[i]){
-        results_df[i,2:4]=NA
-    }
-    }
-}
 return(results_df)
 }
+
+
 ###############################################################
 ##### this code below is for testing
-test1 = cyto_converter(cytobands$band)
+test1 = cyto_converter(bands = cytobands$Band, cytobands = cytobands)
 ## check against UCSC cytoband file and verify output matches exactly
-# table(test1$start==cytobands$start)
-# table(test1$end==cytobands$end)
+# table(test1$Start==cytobands$Start)
+# table(test1$End==cytobands$End)
 #  TRUE TRUE
 ######################################################\
 # form testing script for cyto_converter (all Pters start 0; all qters end at chrom end etc ...)
